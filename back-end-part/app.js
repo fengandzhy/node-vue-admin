17473c237929291b541fbb26d6eeb98f60765ea5
fengandzhy <fengandzhy@gmail.com>
2024-04-25 19:42:56 +1200

    back-end-part basic project born

---
+++ back-end-part/app.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const port = '3000';
+
+let app = http.createServer((req,res)=>{
+    res.end('Hello world');
+});
+
+app.listen(3007, ()=>{
+    console.log('http://127.0.0.1:3007:', port);
+});
