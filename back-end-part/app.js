117b07b4e284b4572f777061d96017ea76c237cb
fengandzhy <fengandzhy@gmail.com>
2024-04-25 19:56:29 +1200

    back-end-part add and import cors

--- back-end-part/app.js
+++ back-end-part/app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
 
-// const http = require('http');
-// const port = '3000';
-//
-// let app = http.createServer((req,res)=>{
-//     res.end('Hello world');
-// });
+// 导入cors中间健
+const cors = require('cors');
+
+// 全局挂载
+app.use(cors());
 
 app.listen(3007, ()=>{
     console.log('http://127.0.0.1:3007');
